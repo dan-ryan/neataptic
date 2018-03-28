493ce4be9f0fc9bd3b1754aa98b2aab74f2ab9ac
Daniel Ryan <daniel.ryan@crichq.com>
2018-03-28 23:03:16 +1300

    efficientMutation flag for neat + XOR test case utilizing it

--- src/neat.js
+++ src/neat.js
@@ -135,7 +135,8 @@ Neat.prototype = {
    * Selects a random mutation method for a genome according to the parameters
    */
   selectMutationMethod: function (genome) {
-    var mutationMethod = this.mutation[Math.floor(Math.random() * this.mutation.length)];
+    var mutation = this.efficientMutation ? genome.getPossibleMutations(this.mutation) : this.mutation;
+    var mutationMethod = mutation[Math.floor(Math.random() * mutation.length)];
 
     if (mutationMethod === methods.mutation.ADD_NODE && genome.nodes.length >= this.maxNodes) {
       if (config.warnings) console.warn('maxNodes exceeded!');
